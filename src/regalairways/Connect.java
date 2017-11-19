9cce4335eda0520afab46a48858d8efc7bcbb1a3
Zeyad <zfaeq@oakland.edu>
2017-11-19 10:14:30 -0500

    test

---
+++ src/regalairways/Connect.java
@@ -0,0 +1,38 @@
+package regalairways;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zeyad
+ */
+public class Connect {
+    public static Connection conn;
+       public static Connection getConnection() throws Exception{
+      try{
+         
+          String driver ="com.mysql.jdbc.Driver";
+          String url ="jdbc:mysql://localhost:3306/lab2?useSSL=false";
+          String username="root";
+          String password="Korgpa600";
+          Class.forName(driver);
+          conn = DriverManager.getConnection(url,username,password);
+          System.out.println("Connected Successfuly!");
+          
+          return conn;
+      }
+      catch(Exception e){
+        System.out.println(e);
+    }
+        return null;
+        
+    }
+}
