dd0d81f32444c511e246546d270e056a9271d433
Dimas Moosa <dimasmoosa@gmail.com>
2017-11-26 17:05:45 -0500

    added buttons for Flight

--- src/regalairways/Connect.java
+++ src/regalairways/Connect.java
@@ -22,7 +22,7 @@ public class Connect {
           String driver ="com.mysql.jdbc.Driver";
           String url ="jdbc:mysql://localhost:3306/regalairways?useSSL=false";
           String username="root";
-          String password="Korgpa600";
+          String password="password";
           Class.forName(driver);
           conn = DriverManager.getConnection(url,username,password);
           System.out.println("Connected Successfuly!");
